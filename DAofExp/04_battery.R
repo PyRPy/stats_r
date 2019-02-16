cdb733083378771cb59eb1dbb2eb7f097ec31850
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-02-16 17:17:44 -0600

    tukey compare, lsmeans
    chapter 4 inferences for contrasts and treatment means

---
+++ DAofExp/04_battery.R
@@ -0,0 +1,31 @@
+# battery.r, battery experiment, Table 4.2, page 97
+
+battery.data <- read.table("Data/battery.txt", header=T)
+battery.data$fType <- factor(battery.data$Type)
+head(battery.data, 5)
+
+summary(battery.data)
+
+model1 <- aov(LPUC ~ fType, data=battery.data) # Fit aov model
+anova(model1) # Display 1-way ANOVA
+
+# Individual contrasts: estimates, CIs, tests
+library(lsmeans)
+lsmType <- lsmeans(model1, ~ fType) # Compute and save lsmeans
+levels(battery.data$fType)
+
+summary(contrast(lsmType, list(Duty=c( 1, 1,-1,-1)/2,
+                              Brand=c( 1,-1, 1,-1)/2,
+                                 DB=c( 1,-1,-1, 1)/2)),
+                          infer=c(T,T), level=0.95, side="two-sided")
+
+# Multiple comparisons
+confint(lsmType, level=0.90) # Display lsmeans and 90
+
+# Tukey's method
+summary(contrast(lsmType, method="pairwise", adjust="tukey"),
+        infer=c(T,T), level=0.95, side="two-sided")
+
+# Dunnett's method
+summary(contrast(lsmType, method="trt.vs.ctrl", adjust="mvt", ref=1),
+        infer=c(T,T), level=0.95, side="two-sided")

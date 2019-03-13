782e2da43f64c2f085a99973f25eb10426e4e016
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-03-13 12:43:52 -0400

    math functions

---
+++ R_easysteps/Ch05_Math.R
@@ -0,0 +1,19 @@
+# math operations
+
+cat( "Pi Constant =", pi, "\n" )
+
+cat( "Pi Floor\t", floor(pi), "\n" )
+cat( "Pi Ceiling\t", ceiling(pi), "\n" )
+cat( "Pi Rounded\t", round(pi, digits=2), "\n\n" )
+
+num <- 64
+cat( "Square Root of", num, "=", sqrt(num),"\n\n" )
+
+num <- ( 120 * ( pi / 180 ) )
+cat( "Cosine of 120° = ",  cos( num ), "\n\n" )
+
+num <- log( 100 )
+cat( "Log of", exp( num ), "=", num, "\n" )
+
+num <- 100
+cat( "Log base 10 of", num , "=", log10( num ), "\n" )

736eda07a0f72e47a76005f33e5353efdf561000
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-03-13 22:03:35 -0400

    random sampling

---
+++ R_easysteps/Ch05_Lottery.R
@@ -0,0 +1,9 @@
+# random sampling method :
+
+nums <- c( 1:59 )
+
+cat( "My Lucky Numbers:", sample( nums, 6 ), "\n\n" )
+
+cat( "My Lucky Numbers:", sample( nums, 6 ), "\n\n" )
+
+# powerball still unreachable !!!

25f5d882dac96325167d12b60edd2b35adb724dc
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-03-14 11:06:31 -0400

    subset for matrix

---
+++ R_easysteps/Ch06_Subset matrix.R
@@ -0,0 +1,18 @@
+# subset for matrix
+
+ny <- c( 3.8, 5.5, 9.9, 15.7, 21.5, 26.3 )
+la <- c( 19.5, 19.4, 19.7, 20.8, 21.3, 22.7 )
+fw <- c( 13.7, 15.4, 20.0, 24.6, 28.5, 32.7 )
+
+table <- cbind( ny, la, fw )
+rownames( table ) <- month.name[ 1:6 ]
+colnames( table ) <- c( "New York", "Los Angeles", "Fort Worth" )
+
+cat( "\nMatrix...\n")
+print( table )
+
+table.q1 <- table[ 1:3, c( 1, 3 ) ]
+
+cat( "\nSubset...\n" )
+print( table.q1 )
+

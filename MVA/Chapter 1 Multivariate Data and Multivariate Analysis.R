15ea5edd72fd4666881d63496f6fa599a080e1ac
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-10-23 17:31:43 -0500

    multivariate analysis
    https://github.com/ryandata
    https://openplantpathology.github.io/OPP_Workshop_Multivariate/#syllabus

---
+++ MVA/Chapter 1 Multivariate Data and Multivariate Analysis.R
@@ -0,0 +1,15 @@
+# Chapter 1 Multivariate Data and Multivariate Analysis
+library(MVA)
+
+# 1.3 Types of variables and the possible problem of ----------------------
+head(hypo)
+hypo[1:2, c("health", "weight")]
+
+# 1.3.1 Missing values
+
+
+# 1.4 Some multivariate data sets -----------------------------------------
+head(measure)
+head(pottery)
+head(exam)
+head(USairpollution)

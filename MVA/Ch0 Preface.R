9f2a9ed314f31439d75f9c36e35a597b458f80c9
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-10-23 17:32:29 -0500

    preface, libraries, data sets

---
+++ MVA/Ch0 Preface.R
@@ -0,0 +1,17 @@
+# preface
+
+# Preface -----------------------------------------------------------------
+
+library(MVA)
+rnorm(10)
+demo("Ch-MVA")
+
+# R> library("MVA")
+# R> demo("Ch-MVA") ### Introduction to Multivariate Analysis
+# R> demo("Ch-Viz") ### Visualization
+# R> demo("Ch-PCA") ### Principal Components Analysis
+# R> demo("Ch-EFA") ### Exploratory Factor Analysis
+# R> demo("Ch-MDS") ### Multidimensional Scaling
+# R> demo("Ch-CA") ### Cluster Analysis
+# R> demo("Ch-SEM") ### Structural Equation Models
+# R> demo("Ch-LME") ### Linear Mixed-Effects Models

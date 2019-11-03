6e3ad18a38c543fc08c9cd17cff6ee30f1151f03
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-11-03 12:28:34 -0600

    add plasma data example : lm vs lme, mixed effects

--- MVA/Chapter 8 The Analysis of Repeated Measures Data.R
+++ MVA/Chapter 8 The Analysis of Repeated Measures Data.R
@@ -36,3 +36,61 @@ exactRLRT(timber.lme)
 anova(timber.lme, timber.lme1)
 
 summary(timber.lme1)
+
+timber$pred1 <- predict(timber.lme1)
+
+# second order of regression
+timber.lme2 <- lme(loads ~ slippage + I(slippage^2),
+                    random = ~slippage | specimen,
+                    data = timber, method = "ML")
+anova(timber.lme1, timber.lme2)
+
+pfun <- function(x, y) {
+   panel.xyplot(x, y[1:length(x)])
+   panel.lines(x, y[1:length(x) + length(x)], lty = 1)
+}
+
+plot(xyplot(cbind(loads, pred1) ~ slippage | specimen,
+             data = timber, panel = pfun, layout = c(4, 2),
+             ylab = "loads"))
+
+# 8.2.3 Fitting random-effect models to the glucose challenge data
+x <- reshape(plasma, direction = "wide", timevar = "time",
+              idvar = "Subject", v.names = "plasma")
+
+# Glucose challenge data for control and obese groups over time. Each line
+# represents the trajectory of one individual.
+parallel(~ x[,-(1:2)] | group, data = x, horizontal = FALSE,
+          col = "black", scales = list(x = list(labels = 1:8)),
+          ylab = "Plasma inorganic phosphate",
+          xlab = "Time (hours after oral glucose challenge)")
+
+plasma.lme1 <- lme(plasma ~ time + I(time^2) + group,
+                       random = ~ time | Subject,
+                       data = plasma, method = "ML")
+
+summary(plasma.lme1)
+# group effect is obtained from 0.437±1.96×0.186, giving [???3.209, 4.083]
+
+
+plot(splom(~ x[, grep("plasma", colnames(x))] | group, data = x,
+            cex = 1.5, pch = ".", pscales = NULL, varnames = 1:8))
+
+# The independence model can be fitted in the usual way with the 
+# lm() function
+
+summary(lm(plasma ~ time + I(time^2) + group, data = plasma))
+
+# the standard error for the group effect is about one-half of that 
+# given for model plasma.lme1 and if used would lead to a much stronger 
+# claim of evidence of a difference between control and obese patients
+
+plasma.lme2 <- lme(plasma ~ time*group +I(time^2),
+                    random = ~time | Subject,
+                    data = plasma, method = "ML")
+
+anova(plasma.lme1, plasma.lme2)
+summary(plasma.lme2)
+
+res.int <- random.effects(plasma.lme2)[,1]
+res.slope <- random.effects(plasma.lme2)[,2]

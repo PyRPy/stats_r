eeadaf16af9352e8c248b604e9a93bd0cdad8497
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-12-28 12:01:48 -0500

    kmeans,  visualization of results

--- MVA/Chapter 6 Cluster Analysis.R
+++ MVA/Chapter 6 Cluster Analysis.R
@@ -67,3 +67,52 @@ sapply(crime_s, var)
 
 # two groups
 kmeans(crime_s, centers = 2)$centers * rge
+n <- nrow(crime_s)
+wss <- rep(0, 6)
+wss[1] <- (n-1)*sum(sapply(crime_s, var))
+for (i in 2:6){
+  wss[i] <- sum(kmeans(crime_s, centers = i)$withinss)
+}
+
+plot(1:6, wss, type="b", xlab="number of groups",
+     ylab="within groups sum of squares")
+
+# 6.4.2 Clustering Romano-British pottery ---------------------------------
+set.seed(29)
+head(pots)
+str(pots)
+# write.csv(pots, "pots.csv")
+pottery_cluster <- kmeans(pots, centers = 3)$cluster
+xtabs(~ pottery_cluster + kiln, data = pottery)
+pots <- scale(pottery[, colnames(pottery_cluster) != "kiln"], center = FALSE)
+pottery_dist <- dist(pots)
+library(lattice)
+levelplot(as.matrix(pottery_dist), xlab="Pot number",
+          ylab = "Pot number") # not working
+
+
+# 6.6 Displaying clustering solutions graphically -------------------------
+
+library(flexclust)
+library(mvtnorm)
+set.seed(290875)
+x <- rbind(rmvnorm(n=20, mean=c(0,0), sigma=diag(2)),
+           rmvnorm(n=20, mean=c(3,3), sigma=0.5*diag(2)),
+           rmvnorm(n=20, mean=c(7,6), sigma=0.5*(diag(2) + 0.25)))
+k <- cclust(x, k=5, save.data = TRUE)
+plot(k, hull=FALSE, col=rep("black", 5), xlab="x", ylab="y")
+
+k <- cclust(x, k=3, save.data = TRUE)
+plot(k, hull=FALSE, col=rep("black", 3), xlab="x", ylab="y")           
+
+# stripes display
+set.seed(912345654)
+x <- rbind(matrix(rnorm(100, sd=0.5), ncol=2),
+           matrix(rnorm(100, mean=4, sd=0.5), ncol=2),
+           matrix(rnorm(100, mean=7, sd=0.5), ncol=2),
+           matrix(rnorm(100, mean=-1.0, sd=0.7), ncol=2),
+           matrix(rnorm(100, mean=-4.0, sd=1.0), ncol=2)
+           )
+
+c5 <- cclust(x, 5, save.data = TRUE)
+stripes(c5, type = "second", col=1)

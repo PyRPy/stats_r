77099fe5ef27478557e39cf2c0205a1e05e3960a
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-01-08 08:59:49 -0600

    sample mean vs population mean - repeat !

--- Sampling/Chapter 2 Simple random sampling.R
+++ Sampling/Chapter 2 Simple random sampling.R
@@ -38,3 +38,40 @@ var(ybar)
 sd(ybar)
 sqrt((1-n/N)*var(y)/n)
 mean((ybar - mu)^2)
+
+
+# Example 4 - sampling data as a test population --------------------------
+library(boot)
+fir
+head(fir)
+y <- fir$count
+y
+table(y)
+
+mean(y)
+
+# Select a random sample without replacement
+s <- sample(1:50, 5) # sample the index
+s
+y[s]
+
+# calculate the sample mean
+mean(y[s]) # true population mean is 2.14
+
+s <- sample(1:50, 5); mean(y[s])
+
+# run the multiple samplings
+ybar <- numeric()
+for (k in 1:10){s <- sample(1:50, 5); ybar[k] <- mean(y[s])}
+ybar
+hist(ybar)
+
+# repeat 1000 times
+ybar <- numeric(1000)
+for (k in 1:1000){s <- sample(1:50, 5); ybar[k] <- mean(y[s])}
+mean(ybar)
+hist(ybar)
+
+# poplulation mean 
+mu <- mean(y)
+mean(ybar) - mu

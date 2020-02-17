ee12448b6780eda7bc41726660edd69a75200fa0
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-02-17 14:37:07 -0600

    simulate it intuitively
    https://stackoverflow.com/questions/22893089/r-how-to-generate-random-sample-of-a-discrete-random-variables

--- ProbStatInference9/binomial_distribution.R
+++ ProbStatInference9/binomial_distribution.R
@@ -1,5 +1,15 @@
-# binomial distribution simulation
-x <- rbinom(10000, 10, 0.5)
-y <- rbinom(10000, 10, 0.2)
-hist(x)
-hist(y)
+
+# Binomial distribution ---------------------------------------------------
+
+a <- 6
+ifelse(rbinom(4, 1, 0.5), -a, a)
+
+# another method
+n <- 10
+X <- rep(0, n)
+Y <- rbinom(n, 1, 1/2)
+X[Y==1] = "a"
+X[Y==0] = "-a"
+X
+Y
+# ref https://stackoverflow.com/questions/22893089/r-how-to-generate-random-sample-of-a-discrete-random-variables

72445be150fe716b185416f491cd245a86ace78a
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-03-04 11:42:16 -0600

    add table 3.1 / table 2.2 with confidence interval

--- Sampling/Chapter 3 Examples.R
+++ Sampling/Chapter 3 Examples.R
@@ -42,3 +42,36 @@ round((tau_hat + qt(c(0.05, 0.95), n-1) * sqrt(var_tau_hat)), 0)
 
 # find 90% CI for the mean
 round((y_hat + qt(c(0.05, 0.95), n-1) * sqrt(var_y_hat)), 1)
+
+# Example 3.2 All possible samples ----------------------------------------
+n = 2
+N = 4
+Unit <- c(1, 2, 3, 4)
+yi <- c(10, 17, 13, 20)
+tau <- sum(yi)
+Ps <- rep(1/choose(4, 2), 6)
+
+# construct data table 2.2
+Sample <- c(12, 13, 14, 23, 24, 34)
+ys_mean <- apply(rbind(yi[c(1,2)], yi[c(1,3)], yi[c(1,4)], yi[c(2,3)], 
+                       yi[c(2,4)], yi[c(3,4)]), 1, mean)
+tau_hat <- ys_mean * N
+ys_sd <- apply(rbind(yi[c(1,2)], yi[c(1,3)], yi[c(1,4)], yi[c(2,3)], 
+                     yi[c(2,4)], yi[c(3,4)]), 1, sd)
+sd2 <- ys_sd^2
+var_tau_hat <- N * (N - n) * sd2 / n
+
+table2.2 <- data.frame(Sample = Sample,
+                       ys_mean = ys_mean,
+                       tau_hat = tau_hat,
+                       sd2 = sd2, 
+                       var_tau_hat = var_tau_hat)
+table2.2
+
+# construct table 3.1 with CI added
+tau_upper <- tau_hat + qt(0.975, n-1)*sqrt(var_tau_hat)
+tau_lower <- tau_hat + qt(0.025, n-1)*sqrt(var_tau_hat)
+table2.2$tau_lower = round(tau_lower, 0)
+table2.2$tau_upper = round(tau_upper, 0)
+
+table2.2

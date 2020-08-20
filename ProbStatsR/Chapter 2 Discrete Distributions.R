78ba12c681bebc7ad4f2291ebc168ba554c0be6d
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-08-20 17:39:02 -0500

    add ex2.6-1 negative binomial distribution

--- ProbStatsR/Chapter 2 Discrete Distributions.R
+++ ProbStatsR/Chapter 2 Discrete Distributions.R
@@ -2,3 +2,18 @@
 
 X <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 sum(X^2)/55 #7
+
+# Ex2.6-1 
+# b) 
+# https://stackoverflow.com/questions/7906332/how-to-calculate-combination-and-permutation-in-r
+perm = function(n, x) {
+  factorial(n) / factorial(n-x)
+}
+
+comb = function(n, x) {
+  factorial(n) / factorial(n-x) / factorial(x)
+}
+
+prob <- comb(29, 2) * 0.1^3 * 0.9^27
+prob # 0.02360879
+

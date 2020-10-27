f266a236fafa4e30283be06f6b353042b80b32e5
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-10-27 17:28:30 -0400

    MANOVA intro on skulls data set

---
+++ AMVA/template_MANOVA_skulls_data.R
@@ -0,0 +1,22 @@
+# Analysis of variance ----------------------------------------------------
+
+data(skulls, package = "HSAUR3")
+means <- aggregate(skulls[,c("mb", "bh", "bl", "nh")], 
+                   list(epoch=skulls$epoch), mean)
+means
+
+pairs(means[,-1], panel = function(x, y) {
+  text(x, y, abbreviate(levels(skulls$epoch)))
+})
+
+
+# MANOVA ------------------------------------------------------------------
+
+skulls_manova <- manova(cbind(mb, bh, bl, nh) ~ epoch, data = skulls)
+summary(skulls_manova, test = "Pillai")
+summary(skulls_manova, test = "Wilks")
+
+summary.aov(skulls_manova)
+
+summary(manova(cbind(mb, bh, bl, nh) ~ epoch, data = skulls,
+               subset = epoch %in% c("c4000BC", "c3300BC")))

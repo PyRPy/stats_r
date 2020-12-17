8c873a77352d1cad5cae5cbbdd925bef1e66ecc1
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-12-17 13:13:58 -0500

    data sets in this book
    Dirk F. Moore
    Department of Biostatistics
    Rutgers School of Public Health
    Piscataway, NJ, USA

---
+++ SurvivalAnalysis/ASAUR_book/Chapter 01 Introduction.R
@@ -0,0 +1,23 @@
+# Chapter 1 Introduction --------------------------------------------------
+
+library(asaur)
+
+# Example 1.2. Xelox in patients with advanced gastric cancer
+gastricXelox[23:27,]
+
+# Example 1.3. Pancreatic cancer in patients with locally advanced or 
+# metastatic disease
+
+head(pancreatic)
+
+# Example 1.4. Survival prospects of prostate cancer patients 
+# with high-risk disease
+
+prostateSurvival[88:95,]
+
+# Example 1.5. Comparison of medical therapies to aid smokers to quit
+pharmacoSmoking[1:6, 2:8]
+
+# Example 1.6. Prediction of survival of hepatocellular carcinoma 
+# patients using biomarkers
+hepatoCellular[c(1, 2, 3, 65, 71),c(2, 3, 16:20, 24, 47)]

024df8fd9447272a6002f7a8732e8be993c110a9
PyRPy <likedatascience@gmail.com>
2020-12-22 12:08:22 -0500

    9.2 Cause-Specific Hazards not tested

--- SurvivalAnalysis/ASAUR_book/Chapter 09 Multiple Survival Outcomes and Competing Risks.R
+++ SurvivalAnalysis/ASAUR_book/Chapter 09 Multiple Survival Outcomes and Competing Risks.R
@@ -43,9 +43,10 @@ result.coxme <- coxme(Surv(time, status) ~ treat +
    as.factor(adult) + treat*as.factor(adult) + (1 | id), data=diabetes)
 summary(result.coxme) 
 
-# # # # # # # # # # # # # # # # # # #
+
+# 9.2 Cause-Specific Hazards ----------------------------------------------
+
 # 9.2.1 competing risks   # prostateSurvival is in "asaur" package
-# # # # # # # # # # # # # # # # # # #
 
 library(survival)
 library(asaur)
@@ -80,7 +81,8 @@ layout.show(3)
 par(mar=c(5,2,2,2), cex.axis=1.65, cex.lab=1.65)
 
 #par(mar=c(5, 4, 4, 4) + 0.1)
-plot(cumDist.prost.km ~ time.km, type="s", ylim=c(0,1), lwd=2, xlab="Years from prostate cancer diagnosis",
+plot(cumDist.prost.km ~ time.km, type="s", ylim=c(0,1), lwd=2, 
+     xlab="Years from prostate cancer diagnosis",
    axes=F, col="blue")
 lines(surv.other.km ~ time.km, type="s", col="green", lwd=2)
 axis(1)
@@ -99,19 +101,17 @@ text(x=0.1,y=2.5,"Probability of death from prostate cancer",srt=90,cex=1.65)
 plot(x ~ y,type="n",axes=F)
 text(x=0.15,y=2.5,"Probability of death from other causes",srt=-90,cex=1.65)
 
-
-# # # # # # # # # # # #
 # Section 9.2.2 cause-specific hazards and cumulative incidence functions
-# # # # # # # # # # # #
-
 tt <- c(2,7,5,3,4,6)
 status <- c(1,2,1,2,0,0) 
 status.any <- as.numeric(status >= 1) 
 result.any <- survfit(Surv(tt, status.any) ~ 1) 
 result.any$surv 
 
-install.packages("mstate") # must be done once
+# install.packages("mstate") # must be done once
 library(mstate)
+
+# The following codes are not tested --------------------------------------
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # #  ! !
 # # # # # # # # # # # # # # # # # # # # # # # # # # # #  ! !
@@ -121,7 +121,8 @@ library(mstate)
 #  cumulative incidence plots are too coarse
 #
 # Thus, it is best to use 
-#  survfit(Surv(survTime, status, type="mstate") ~ 1, data=prostateSurvival.highrisk)
+#  survfit(Surv(survTime, status, type="mstate") ~ 1, 
+#            data=prostateSurvival.highrisk)
 # as described below
 # # # # # # # # # # # # # # # # # # # # # # # # # # # #  ! !
 # # # # # # # # # # # # # # # # # # # # # # # # # # # #  ! !
@@ -149,7 +150,8 @@ rm(status)
 #times <- ci.prostate$time/12  # convert months to years 
 
 # use this code instead:
-sf <- survfit(Surv(survTime, status, type="mstate") ~ 1, data=prostateSurvival.highrisk)
+sf <- survfit(Surv(survTime, status, type="mstate") ~ 1, 
+              data=prostateSurvival.highrisk)
 tt <- sf$time
 CIs <- sf$prev
 SSurv <- 1 - apply(CIs, 1, sum)
@@ -159,7 +161,8 @@ times <- tt/12
 
 Rci2 <- 1 - ci2
 
-# figure 9.4   (simple)       # works for R version 3.2, perhaps not for version 3.3
+# figure 9.4   (simple)       
+# works for R version 3.2, perhaps not for version 3.3
 windows()
 plot(Rci2 ~ times, type="s", ylim=c(0,1), lwd=2, col="green",
     xlab="Time in years", ylab="Survival probability",
@@ -168,7 +171,8 @@ lines(ci1 ~ times, type="s", lwd=2, col="blue")
 lines(surv.other.km ~ time.km, type="s", col="lightgreen", lwd=1)
 lines(cumDist.prost.km ~ time.km, type="s", col="lightblue", lwd=1) 
 
-# Figure 9.4 (more complete)    # works for R version 3.2, perhaps not for version 3.3
+# Figure 9.4 (more complete)    
+# works for R version 3.2, perhaps not for version 3.3
 windows(height=5, width=7)
 
 layout(matrix(c(2,1,3), ncol=3),
@@ -177,7 +181,8 @@ layout.show(3)
 par(mar=c(5,2,2,2), cex.axis=1.65, cex.lab=1.65)
 
 #par(mar=c(5, 4, 4, 4) + 0.1)
-plot(cumDist.prost.km ~ time.km, type="s", ylim=c(0,1), lwd=1, xlab="Years from prostate cancer diagnosis",
+plot(cumDist.prost.km ~ time.km, type="s", ylim=c(0,1), lwd=1, 
+     xlab="Years from prostate cancer diagnosis",
    axes=F, col="lightblue", xlim=c(0,10))
 lines(surv.other.km ~ time.km, type="s", col="lightgreen", lwd=1)
 axis(1)
@@ -204,7 +209,8 @@ windows(height=5, width=7)
 
 par(mar=c(5, 4, 2, 2) + 0.1, cex.lab=1.1, cex.axis=1.1)  # default
 
-plot(ci1 ~ times, type="s", ylim=c(0,1), lwd=2, xlab="Years from prostate cancer diagnosis",
+plot(ci1 ~ times, type="s", ylim=c(0,1), lwd=2, 
+     xlab="Years from prostate cancer diagnosis",
    ylab="Probability patient has died", axes=F, col="blue", xlim=c(0,10))
 ci1.ci2 <- ci1 + ci2
 lines(ci1.ci2 ~ times, type="s", lwd=2, col="green")
@@ -232,7 +238,7 @@ summary(result.other)
 cov.matrix <- model.matrix(~ grade + ageGroup)
 head(cov.matrix)
 
-install.packages("cmprsk") # must be done once
+# install.packages("cmprsk") # must be done once
 library(cmprsk)
 result.prostate.crr <- crr(survTime, status, cov1=cov.matrix[,-1], failcode=1) 
 summary(result.prostate.crr)
@@ -253,10 +259,15 @@ prostate.long <- msprep(time = cbind(NA, survTime, survTime),
 head(prostate.long) 
 events(prostate.long) 
 
-summary(coxph(Surv(time, status) ~ grade + ageGroup, data=prostate.long, subset={trans==1}))
-summary(coxph(Surv(time, status) ~ grade + ageGroup, data=prostate.long, subset={trans==2}))
+summary(coxph(Surv(time, status) ~ grade + ageGroup, 
+              data=prostate.long, subset={trans==1}))
+summary(coxph(Surv(time, status) ~ grade + ageGroup, 
+              data=prostate.long, subset={trans==2}))
 
-summary(coxph(Surv(time, status) ~ grade + ageGroup + strata(trans), data=prostate.long))
-summary(coxph(Surv(time, status) ~ grade*factor(trans) + ageGroup + strata(trans), data=prostate.long))  
-summary(coxph(Surv(time, status) ~ (grade + ageGroup)*trans + ageGroup + strata(trans), data=prostate.long)) 
+summary(coxph(Surv(time, status) ~ grade + ageGroup + strata(trans), 
+              data=prostate.long))
+summary(coxph(Surv(time, status) ~ grade*factor(trans) + ageGroup + strata(trans), 
+              data=prostate.long))  
+summary(coxph(Surv(time, status) ~ (grade + ageGroup)*trans + ageGroup + strata(trans), 
+              data=prostate.long)) 
 

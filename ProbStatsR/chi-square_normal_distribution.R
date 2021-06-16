57e7fcc6e10a54772f8e6d96850f9514f0e05f9d
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-06-16 13:36:36 -0500

    relationship between chi-square and normal distri.
    https://online.stat.psu.edu/stat414/lesson/16/16.5

---
+++ ProbStatsR/chi-square_normal_distribution.R
@@ -0,0 +1,10 @@
+# chi-square and standard normal distribution
+# find probability between Z = -1.96 and Z = 1.96
+# for normal distribution
+pnorm(1.96) - pnorm(-1.96)  # 0.9500042
+
+# for chi-square
+pchisq(1.96^2, 1) # 0.9500042, 1 degree of freedom
+
+
+

5119e9ad6f316f00c42360016c6d238ed10b4c94
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-11-29 17:16:05 -0500

    correction on 'rank'

--- ProbStatsR/E8_5_10.R
+++ ProbStatsR/E8_5_10.R
@@ -9,8 +9,8 @@ dat
 
 dat_long = c(dat$x, dat$y)
 dat_long
-dat_order = order(dat_long)
-dat_order
+dat_rank = rank(dat_long) # correction
+dat_rank
 
 # dat_long <- scan('Data415/E8_5-10.txt')
 # tree = c(rep('x', 12), rep('y', 12))
@@ -19,12 +19,13 @@ dat_order
 # dat_order = order(dat_label$dat_long)
 # dat_order
 
-W = sum(dat_order[1:12])
+W_x = sum(dat_rank[1:12])
+W_y = sum(dat_rank[13:24])
 nx = 12
 ny = 12
 
 mu_W = nx * ( nx + ny + 1) / 2
 Z = (W - mu_W) / sqrt(nx*ny*(nx+ny+1)/12) # -2.19
 
-
-
+# base R function
+wilcox.test(dat$x, dat$y, alternative = "two.sided")

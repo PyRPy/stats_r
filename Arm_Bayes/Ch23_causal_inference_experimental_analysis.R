9f6cf923326f9d59e369424adeed1dcf58ba9a69
PyRPy <44626163+PyRPy@users.noreply.github.com>
2023-09-19 10:39:58 -0500

    instrumental variable and intent-to-treat concept

--- Arm_Bayes/Ch23_causal_inference_experimental_analysis.R
+++ Arm_Bayes/Ch23_causal_inference_experimental_analysis.R
@@ -120,3 +120,100 @@ params <- c("theta","a","mu.a","sigma.y","sigma.a", "b.pre.test")
 electric.ch23.1c <- bugs (data, inits, params, model.ch23.1c, n.iter=5000,
                           debug=TRUE )
 plot (electric.ch23.1c)
+
+
+# 23.4 Instrumental variables and multilevel modeling ---------------------
+
+## Read the data & redefine variables
+# Data are at http://www.stat.columbia.edu/~gelman/arm/examples/sesame
+
+library(foreign)
+sesame <- read.dta("ARM_Data/sesame/sesame.dta")
+head(sesame)
+
+y=sesame$postlet
+d=sesame$regular
+yt=cbind(sesame$postlet,sesame$regular)
+z=sesame$encour
+n=nrow(sesame)
+
+siteset=numeric(nrow(sesame))
+for(j in 1:2){
+  for(i in 1:5){
+    siteset[sesame$site==i & sesame$setting==j]=i+5*(j-1)
+  }
+}
+J=9
+
+## Fit the model for example 1
+model.ses <- function() {
+  for (i in 1:n){
+    yt[i,1:2] ~ dmnorm (yt.hat[i,],Tau.yt[,])      # data model
+    yt.hat[i,1] <- a[siteset[i]] + b*yt[i,2]
+    yt.hat[i,2] <- g[siteset[i]] + d*z[i]
+  }
+  for (j in 1:J){
+    ag[j,1:2] ~ dmnorm (mu.ag[1:2],Tau.ag[1:2,1:2])
+    a[j] <- ag[j,1]
+    g[j] <- ag[j,2]
+  }
+
+  # data level
+  Tau.yt[1:2,1:2] <- inverse(Sigma.yt[,])
+  Sigma.yt[1,1] <- pow(sigma.y,2)
+  sigma.y ~ dunif (0, 100)                  # noninformative prior on sigma.a
+  Sigma.yt[2,2] <- pow(sigma.t,2)
+  sigma.t ~ dunif (0, 100)                  # noninformative prior on sigma.b
+  Sigma.yt[1,2] <- rho.yt*sigma.y*sigma.t
+  Sigma.yt[2,1] <- Sigma.yt[1,2]            # noninformative prior on rho
+  rho.yt ~ dunif(-1,1)
+  d ~ dnorm (0, .001)
+  b ~ dnorm (0, .001)
+
+  # group level
+  Tau.ag[1:2,1:2] <- inverse(Sigma.ag[,])
+  Sigma.ag[1,1] <- pow(sigma.a,2)
+  sigma.a ~ dunif (0, 100)
+  Sigma.ag[2,2] <- pow(sigma.g,2)
+  sigma.g ~ dunif (0, 100)
+  Sigma.ag[1,2] <- rho.ag*sigma.a*sigma.g
+  Sigma.ag[2,1] <- Sigma.ag[1,2]
+  rho.ag ~ dunif(-1,1)
+
+  mu.ag[1] ~ dnorm (0, .001)
+  mu.ag[2] ~ dnorm (0, .001)
+}
+ses.data <- list("yt","z","n","siteset","J")
+ses.params <- c("a","g","d","b","sigma.y","sigma.t","rho.yt","sigma.g",
+                "sigma.a","rho.ag")
+
+ses.inits <- function(){
+  list(d=rnorm(1,.35,.1),b=rnorm(1),sigma.y=runif(1),sigma.t=runif(1),
+       rho.yt=runif(1,-1,1),sigma.a=runif(1),sigma.g=runif(1),
+       rho.ag=runif(1,-1,1),ag=cbind(rnorm(J),rnorm(J)))
+}
+
+ses <- bugs(ses.data,
+            ses.inits,
+            ses.params,
+            model.ses,
+            n.chains=3,
+            n.iter=1000,
+            debug=TRUE )
+print(ses)
+# some of the Rhat is higher than 1.1
+#            mean  sd   2.5%    25%    50%    75%  97.5% Rhat n.eff
+# a[1]       11.0 5.0    1.0    7.5   11.5   14.5   20.0  1.5     7
+# a[2]       22.7 4.8   12.8   19.7   22.9   26.0   31.9  1.3    11
+# a[3]        8.6 4.4   -0.2    5.4    9.3   11.7   16.4  1.6     7
+# g[8]        0.6 0.1    0.5    0.6    0.6    0.7    0.8  1.0  1500
+# g[9]        0.2 0.1    0.0    0.1    0.2    0.3    0.4  1.1    27
+# d           0.3 0.1    0.2    0.3    0.3    0.4    0.4  1.0    79
+# b          14.3 5.0    5.4   10.9   13.5   18.1   24.6  1.5     8
+# sigma.y    10.9 0.7    9.5   10.6   11.0   11.2   12.7  1.1    30
+# sigma.t     0.4 0.0    0.3    0.3    0.4    0.4    0.4  1.1    26
+# rho.yt     -0.1 0.2   -0.4   -0.3   -0.1    0.0    0.2  1.9     5
+# sigma.g     0.7 1.0    0.1    0.2    0.2    0.4    3.5  1.9     6
+# sigma.a     5.4 1.8    2.8    4.1    5.2    6.4   10.2  1.9     6
+# rho.ag      0.0 0.3   -0.5   -0.2    0.0    0.2    0.6  1.1    49
+# deviance 2000.8 8.4 1987.5 1995.0 2000.0 2006.0 2020.0  1.0    66

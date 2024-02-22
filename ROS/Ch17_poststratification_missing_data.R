dad3fb34e72400de91c1c90426ac7ade160bf79c
PyRPy <44626163+PyRPy@users.noreply.github.com>
2024-02-22 11:18:40 -0600

    explore methods of imputation for missing data

--- ROS/Ch17_poststratification_missing_data.R
+++ ROS/Ch17_poststratification_missing_data.R
@@ -2,6 +2,7 @@
 # 17. POSTSTRATIFICATION AND MISSING-DATA IMPUTATION ----------------------
 # https://avehtari.github.io/ROS-Examples/Poststrat/poststrat2.html
 # https://online.stat.psu.edu/stat506/lesson/6/6.3
+# https://avehtari.github.io/ROS-Examples/Imputation/imputation.html
 # Load packages
 library("rstanarm")
 invlogit <- plogis
@@ -96,3 +97,108 @@ round(poststrat_est, 2)
 # plus uncertainty
 poststrat_sim <- pred_sim %*% poststrat$N / sum(poststrat$N)
 round(c(mean(poststrat_sim), sd(poststrat_sim)), 3)
+
+
+
+# MISSING-DATA IMPUTATION -------------------------------------------------
+library("rstanarm")
+
+# data
+SIS <- read.csv("ROS_Data/SIS.csv")
+head(SIS)
+
+# helper functions for imputation
+impute <- function(a, a_impute) {
+  ifelse(is.na(a), a_impute, a)
+}
+
+topcode <- function(a, top) {
+  ifelse(a > top, top, a)
+}
+
+# Deterministic imputation
+# Impute 0 earnings using the logical rule (if worked 0 months and 0 hrs/wk)
+SIS$earnings_top <- topcode(SIS$earnings, 100)
+SIS$earnings_top[SIS$workhrs_top==0 & SIS$workmos==0] <- 0
+
+# Create a dataset with all predictor variables
+n <- nrow(SIS)
+SIS_predictors <- SIS[,c("male","over65","white","immig","educ_r","workmos",
+                         "workhrs_top","any_ssi","any_welfare","any_charity")]
+
+# Impute subset of earnings that are nonzero: linear scale
+fit_imp_1 <- stan_glm(
+  earnings ~ male + over65 + white + immig + educ_r +
+    workmos + workhrs_top + any_ssi +
+    any_welfare + any_charity,
+  data = SIS,
+  subset = earnings > 0,
+  refresh = 0
+)
+print(fit_imp_1)
+
+# point predictions
+pred_1 <- colMeans(posterior_linpred(fit_imp_1, newdata = SIS_predictors))
+SIS$earnings_imp_1 <- impute(SIS$earnings, pred_1)
+
+# Impute subset of earnings that are nonzero: square root scale and topcoding
+fit_imp_2 <- stan_glm(
+  sqrt(earnings_top) ~ male + over65 + white + immig +
+    educ_r + workmos + workhrs_top + any_ssi +
+    any_welfare + any_charity,
+  data = SIS,
+  subset = earnings > 0,
+  refresh = 0
+)
+print(fit_imp_2)
+
+# point predictions
+pred_2_sqrt <- colMeans(posterior_linpred(fit_imp_2, newdata = SIS_predictors))
+pred_2 <- topcode(pred_2_sqrt^2, 100)
+SIS$earnings_imp_2 <- impute(SIS$earnings_top, pred_2)
+
+# One random imputation
+# Linear scale (use fitted model fit_imp_1)
+pred_3 <- posterior_predict(fit_imp_1, newdata = SIS_predictors, draws = 1)
+SIS$earnings_imp_3 <- impute(SIS$earnings, pred_3)
+
+# Square root scale and topcoding (use fitted model fit_imp_2)
+pred_4_sqrt <- posterior_predict(fit_imp_2, newdata = SIS_predictors, draws = 1)
+pred_4 <- topcode(pred_4_sqrt^2, 100)
+SIS$earnings_imp_4 <- impute(SIS$earnings_top, pred_4)
+
+# Iterative regression imputation
+# Starting values
+random_imp <- function (a){
+  missing <- is.na(a)
+  n_missing <- sum(missing)
+  a_obs <- a[!missing]
+  imputed <- a
+  imputed[missing] <- sample(a_obs, n_missing)
+  imputed
+}
+SIS$interest_imp <- random_imp(SIS$interest)
+SIS$earnings_imp <- random_imp(SIS$earnings)
+
+# Simplest regression imputation
+n_loop <- 10
+for (s in 1:n_loop){
+  fit <- stan_glm(earnings ~ interest_imp + male + over65 + white +
+                    immig + educ_r + workmos + workhrs_top + any_ssi + any_welfare +
+                    any_charity, data=SIS, refresh = 0)
+  SIS_predictors <- SIS[,c("male","over65","white","immig","educ_r","workmos",
+                           "workhrs_top","any_ssi","any_welfare","any_charity",
+                           "interest_imp", "earnings_imp")]
+  pred1 <- posterior_predict(fit, newdata = SIS_predictors, draws = 1)
+  SIS$earnings_imp <- impute(SIS$earnings, pred1)
+
+  fit <- stan_glm(interest ~ earnings_imp + male + over65 + white +
+                    immig + educ_r + workmos + workhrs_top + any_ssi + any_welfare +
+                    any_charity, data=SIS, refresh = 0)
+  SIS_predictors <- SIS[,c("male","over65","white","immig","educ_r","workmos",
+                           "workhrs_top","any_ssi","any_welfare","any_charity",
+                           "interest_imp", "earnings_imp")]
+  pred2 <- posterior_predict(fit, newdata = SIS_predictors, draws = 1)
+  SIS$interest_imp <- impute(SIS$interest, pred2)
+}
+head(SIS)

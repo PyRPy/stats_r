068b5222d03dbadb0b87ad80a93b460a78be1b44
PyRPy <44626163+PyRPy@users.noreply.github.com>
2024-07-11 11:35:17 -0500

    experimental design with computational model

--- DAExp/Chapter11_Response Surface Methods and Designs.R
+++ DAExp/Chapter11_Response Surface Methods and Designs.R
@@ -34,3 +34,44 @@ summary(model)$lof
 
 #  response surface plots
 contour(model, ~ x1 + x2, image = TRUE)
+
+# D and I optimal design  -------------------------------------------------
+library(Vdgraph)
+head(Table11.13)
+FDSPlot(Table11.13[,c('X1','X2','X3','X4')], mod=2) # D
+
+head(Table11.14)
+FDSPlot(Table11.14[,c('X1','X2','X3','X4')], mod=2) # I
+
+
+# Numerical experimental design with computational model ------------------
+
+# jet engine exahust fume temperature
+library(DiceDesign)
+#  Latin Hypercube design
+
+df <- lhsDesign(10, 2)$design
+colnames(df) <- c('x', 'R')
+# Translate the points which are on [0,1] scale to the required scale
+df[,'x'] <- df[,'x'] * 0.9 + 0.05
+df[,'R'] <- df[,'R'] * 0.062
+plot(df, main='LHS Design')
+
+head(Table11.16) # simulation results
+
+# fitting the model
+library(DiceEval)
+model <- modelFit(Table11.16[,c('x','R')],
+                  Table11.16[,'Temperature'],
+                  type='Kriging')
+
+# contour plot
+x.range<-range(Table11.16[,'x'])
+R.range<-range(Table11.16[,'R'])
+x<-seq(from=x.range[1],to=x.range[2],length.out=100)
+R<-seq(from=R.range[1],to=R.range[2],length.out=100)
+z<-matrix(
+  modelPredict(model,expand.grid('x'=x, 'R'=R)),
+  nrow=length(x)
+)
+contour(x,R,z,main='GaussianProcessMeanSurface')

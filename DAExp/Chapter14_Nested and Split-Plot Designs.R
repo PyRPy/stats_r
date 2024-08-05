614f525991d16100ad524c082fdb0c2c1902aecb
PyRPy <44626163+PyRPy@users.noreply.github.com>
2024-08-05 11:39:51 -0500

    add a split-plot design example
    paper strength

--- DAExp/Chapter14_Nested and Split-Plot Designs.R
+++ DAExp/Chapter14_Nested and Split-Plot Designs.R
@@ -17,3 +17,35 @@ model <- lmer(Purity ~ (1 | Supplier/Batches), data=Table14.3)
 print(summary(model))
 
 print(rand(model)) # most variance comes from within suppliers
+
+
+# Circuit insertion -  assembly time vs fixture, layout, operator ---------
+head(Table14.10)
+df <- Table14.10
+df[,'FO'] <- factor(sprintf('F%s.O%s', as.character(df[,'Fixture']),
+                            as.character(df[,'Operator'])))
+head(df)
+# pay attention to the levels of the data structure
+model <- lmer(AssemblyTime ~ Layout * Fixture + (1|Operator/Layout) +
+                (1|FO/Layout), data=df)
+print(anova(model))
+print(summary(model))
+print(rand(model)) # none is significant?
+
+
+
+# Strength of paper -------------------------------------------------------
+# factors : prepparation method, replicates, temperature
+head(Table14.16)
+df<-Table14.16
+df[,'Temperature']<-factor(df[,'Temperature'])
+
+# fixed effect model
+model<-aov(TensileStrength ~ Temperature*Preperation + Error(Replicate),data=df)
+summary(model)
+
+# mixed effect model - different than in textbook
+model<-lmer(TensileStrength~Temperature*Preperation*(1|Replicate),data=df)
+print(anova(model))
+print(summary(model))
+print(rand(model))
